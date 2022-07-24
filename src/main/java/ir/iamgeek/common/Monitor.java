5fd0f855386a934b02f3a09b365e30ed72cbdc80
Fatemeh Ghasemi <fghasemi180@gmail.com>
2022-07-24 18:07:46 +0430

    start adding connection on monitor method

--- src/main/java/ir/iamgeek/common/Monitor.java
+++ src/main/java/ir/iamgeek/common/Monitor.java
@@ -6,21 +6,23 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.sql.Connection;
 
 @Aspect
 @Component
 public class Monitor {
     @Around("execution(* ir.iamgeek.repository.PersonDA.* (..))")
-    public void monitor(ProceedingJoinPoint point) throws Throwable{
+    public Object monitor(ProceedingJoinPoint point) throws Throwable {
         Method declaredMethod = point.getTarget().getClass().getDeclaredMethod(point.getSignature().getName());
         Transactional annotation = declaredMethod.getDeclaredAnnotation(Transactional.class);
-        if (annotation!=null){
-            point.proceed();
+        if (annotation != null) {
+            Connection connection = JDBC.getConnection();
+            Object proceed = point.proceed(new Object[]{connection});
+            connection.close();
+            return proceed;
         }
-
-
+        return null;
     }
 
 }
--- src/main/java/ir/iamgeek/repository/PersonDA.java
+++ src/main/java/ir/iamgeek/repository/PersonDA.java
@@ -1,7 +1,6 @@
 package ir.iamgeek.repository;
 
 import ir.iamgeek.annotation.Transactional;
-import ir.iamgeek.entity.Person;
 import org.springframework.stereotype.Repository;
 
 @Repository
